1ba9c5fa40d47d3cd7a3d4cce630acbdf60d920f
Tracy H <aag9n17o@duck.com>
2024-01-28 17:36:23 -0600

    Add blog, update settings, and add superuser.

--- djangofest/settings.py
+++ djangofest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xyl2p2)p%#hgai5j55po7^9s-u^1-9sf_a083v2i@)f=&p-n3v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.2', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
