3e64221d701ce36049a05f562ce8df4db21729e4
Tracy H <aag9n17o@duck.com>
2024-01-28 19:20:15 -0600

    Reset debug in settings.py. Add type = text/css to blog.css ref in post_list.html.

--- djangofest/settings.py
+++ djangofest/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xyl2p2)p%#hgai5j55po7^9s-u^1-9sf_a083v2i@)f=&p-n3v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.2', '.pythonanywhere.com']
 
